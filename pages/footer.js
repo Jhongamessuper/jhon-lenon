93db281ba0f488e57f09864d56af9c067538f997
Jhon Lenon <Jhongamessuper564email@gmail.com>
2022-10-06 09:02:02 -0300

    Teste

--- pages/footer.js
+++ pages/footer.js
@@ -1,13 +1,10 @@
 export default function Footer(){
     return (
         <>
-          <p>A principal razão que leva uma pessoa a estudar os mitos são os frutos simbólicos que têm para entender mais profundamente o ser humano. Quando pensamos na importância da mitologia grega, deparamo-nos também com a influência na literatura e nas artes, assim como a influência na filosofia e na psicologia.
-
-A Grécia é considerada o berço da democracia e legou ao Ocidente: a Filosofia, as epopeias com heróis e deuses, o conhecimento dos princípios matemáticos e as bases do teatro com suas tragédias e comédias.
-
-Neste artigo estudaremos a importância da Mitologia Grega, veremos os detalhes de sua origem e desenvolvimento, além de seu legado para todos nós atualmente.
-
-Se você estiver interessado em assuntos históricos e filosóficos, acompanhe nossos documentários gratuitos. Neles, você encontrará trechos de entrevistas com grandes professores, muitos deles, de história e filosofia.</p>
+          <p>A principal razão que leva uma pessoa a estudar os mitos são os frutos simbólicos que têm para entender mais profundamente o ser humano. Quando pensamos na importância da mitologia grega, deparamo-nos também com a influência na literatura e nas artes, assim como a influência na filosofia e na psicologia.<br></br>
+             A Grécia é considerada o berço da democracia e legou ao Ocidente: a Filosofia, as epopeias com heróis e deuses, o conhecimento dos princípios matemáticos e as bases do teatro com suas tragédias e comédias.<br></br>
+             Neste artigo estudaremos a importância da Mitologia Grega, veremos os detalhes de sua origem e desenvolvimento, além de seu legado para todos nós atualmente.<br></br>
+             Se você estiver interessado em assuntos históricos e filosóficos, acompanhe nossos documentários gratuitos. Neles, você encontrará trechos de entrevistas com grandes professores, muitos deles, de história e filosofia.</p>
         </>
   )
 }
